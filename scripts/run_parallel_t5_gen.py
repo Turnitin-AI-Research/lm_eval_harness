77ee05a1b62a347583ce28c99461131f2e8cbc65
Sumeet Singh <ssingh@turnitin.com>
2023-01-18 12:33:25 -0800

    Updated run scripts.

--- scripts/run_parallel_t5_gen.py
+++ scripts/run_parallel_t5_gen.py
@@ -1,8 +1,18 @@
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import itertools
-from main import main
+import ray
 
+
+# DEPRICATED: DO NOT submit jobs to a cluster because jobs will be run in the environment in which the cluster was started.
+# run "ray start --head --dashboard-host 0.0.0.0" from the repo root directory from within the venv lme.
+# If you to attach another machine to the cluster, then run "ray start --address=<head-node-ip>:6379" there.
+# To view dashboard, forward local port to remote dashboard either using vscode or via ssh: ssh -L 8265:<head-node-ip>:8265 <head-node-ip>
+# ray.init(address='auto')
+
+# Start a new cluster in order to ensure we're using the right environment. This will prevent us from connecting to a running
+# ray cluster that was started in another environment.
+ray.init(address='local')
 
 results_dir = "lmeval_results_t5/"
 num_fewshots = [5]
@@ -15,9 +25,9 @@ word_agg_schemes = ['-relu|mean', '-relu+|mean', 'mean', 'relu+|mean', 'relu|mea
 segment_agg_schemes = [None, 'mean']
 example_agg_schemes = [None, 'mean']
 norms = ['layer', None]
-sim_funcs = ['dot_product']
+sim_funcs = [None]  # ['dot_product']
 # ['middle', None, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
-encoding_layers = [0]
+encoding_layers = [None]
 parallelize = True
 
 if 0 in num_fewshots:
@@ -26,10 +36,19 @@ if 0 in num_fewshots:
     assert ALLOWED_ZEROSHOT_EXAMPLE_AGG_SCHEMES & set(example_agg_schemes)
     assert ALLOWED_ZEROSHOT_ENCODING_SCHEMES & set(encoding_schemes)
 
-responses = []
+
+@ray.remote(max_calls=1, num_gpus=2)
+# @ray.remote(max_calls=1, num_cpus=4)
+def run_eval(args):
+    os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+    from main import main
+    return main(*args)
+
+
 os.makedirs(results_dir, exist_ok=True)
+futures = []
 for num_fewshot, (task, model), submodel, encoding_scheme, word_agg_scheme, segment_agg_scheme, example_agg_scheme, norm, sim_func, encoding_layer in itertools.product(
-        num_fewshots, task_models, pretrained, encoding_schemes, word_agg_schemes, segment_agg_schemes, example_agg_schemes, norms, sim_funcs, encoding_layers):
+    num_fewshots, task_models, pretrained, encoding_schemes, word_agg_schemes, segment_agg_schemes, example_agg_schemes, norms, sim_funcs, encoding_layers):
 
     if num_fewshot == 0:
         if ((encoding_scheme not in ALLOWED_ZEROSHOT_ENCODING_SCHEMES)
@@ -51,7 +70,13 @@ for num_fewshot, (task, model), submodel, encoding_scheme, word_agg_scheme, segm
     if submodel is not None:
         model_args = model_args + f',pretrained={submodel}'
     _args.extend(['--model_args', model_args])
-    response = main(*_args)
-    responses.append(response[1])
+    future = run_eval.remote(_args)
+    futures.append(future)
+
+responses = ray.get(futures)
+# for resp in responses:
+#     fpath, results = resp
+#     with open(fpath, "wt", encoding='utf-8') as f:
+#         json.dump(results, f, indent=2)
 
 print(responses)
