ff52114265f613b2f083a5d0ccdb9f0db13703c7
Sumeet Singh <ssingh@turnitin.com>
2023-01-17 20:10:28 -0800

    added run_parallel_t5.py

--- tests/test_dist3.py
+++ tests/test_dist3.py
@@ -29,8 +29,39 @@ def _test(limit: int = 100, device: str = "7", model_parallel: bool = False):
     fpath, results = run_eval(*_args)
     print(results)
 
-    # assert results['results']['hellaswag_d']['acc'] == expected, f"Test Failed: expected={expected}, got={results['results']['hellaswag_d']['acc']}"
-    # print('Test Passed')
+    if limit is None:
+        # {
+        # "results": {
+        #     "hellaswag_d": {
+        #     "acc": 0.30611431985660226,
+        #     "acc_stderr": 0.004599358920909542,
+        #     "rand_acc": 0.25,
+        #     "rand_acc_stderr": 0.0
+        #     }
+        # },
+        # "versions": {
+        #     "hellaswag_d": 0
+        # },
+        # "config": {
+        #     "model": "dist_sim",
+        #     "model_args": "WORD_AGG_SCHEME=relu|mean,EXAMPLE_AGG_SCHEME=None,SEGMENT_AGG_SCHEME=None,NORM=layer,SIMILARITY_FUNC=dot_product,pretrained=EleutherAI/gpt-neo-1.3B,ENCODING_LAYER=23,PARALLELIZE=False",
+        #     "task_args": "encoding_scheme=sentence_level_segmentation",
+        #     "num_fewshot": 0,
+        #     "batch_size": null,
+        #     "device": "7",
+        #     "no_cache": true,
+        #     "limit": null,
+        #     "bootstrap_iters": 100000,
+        #     "description_dict": {}
+        # }
+        # }
+        expected = 0.3061
+    else:
+        raise ValueError(f'Unsupported limit value {limit}')
+
+    assert results['results']['hellaswag_d']['acc'] == expected, f"Test Failed: expected={expected}, got={results['results']['hellaswag_d']['acc']}"
+    print('Test Passed')
+
 
 if __name__ == '__main__':
     fire.Fire(_test)
