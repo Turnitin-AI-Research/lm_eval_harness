a50072ad2c7b234bf600f5bbd2d630a7d672aa43
ssingh <ssingh@turnitin.com>
2023-06-15 18:18:25 -0700

    Updated GPU-requirement for enc-dec models.

--- scripts/run_utils.py
+++ scripts/run_utils.py
@@ -27,7 +27,7 @@ NUM_ENCDEC_GPUS_BY_MODEL_SIZE = {
     3500: 2,  # 2x 11GB GPU
     6500: 2,  # 3x 11GB GPU
     7500: 2,  # 3x 11GB GPU
-    11500: 2,  # 2x 24GB GPUs
+    11500: 4,  # 4x 11GB GPUs
     13000: 3,  # 3x 24GB GPUs
     20000: 4,  # 4x 24GB GPUs
 }
