11b050d00eac667420acf79e622d6e3f55a09bf9
Sumeet Singh <ssingh@turnitin.com>
2023-07-07 09:46:33 -0700

    minor

--- scripts/run_parallel_sim_gpt.py
+++ scripts/run_parallel_sim_gpt.py
@@ -5,27 +5,27 @@ import ray
 import fire
 import scripts.run_utils as utils
 from main import results_fpath
+import logging
 
 
 def run(overwrite_results: bool, NUM_GPUS_PER_RUN: int, cluster: str):
     utils.ray_init(num_gpus_per_run=NUM_GPUS_PER_RUN, cluster=cluster)
     results_dir = "lmeval_results_sim_latest/"
-    num_fewshots = [0]
+    num_fewshots = [5, 0]
     task_models = [('hellaswag_d', 'dist_sim')]  # ('hellaswag_d', 'dist_sim'), ('webqs_dg', 'dist_gen')]
-    # utils.get_models(arch_type='Decoder Only', max_size=11000)  # ['EleutherAI/gpt-neo-2.7B']  # ['EleutherAI/gpt-neo-1.3B',]
-    pretrained = ['EleutherAI/gpt-neo-2.7B', 'EleutherAI/pythia-2.8b-deduped']
+    pretrained = utils.get_models(arch_type='Decoder Only', max_size=8000)
     parallelize: bool = True
     # ['merge_all_segments', 'segment_each_example', 'concat_each_example', 'concat_all_examples']
-    encoding_schemes = ['sentence_level_segmentation', 'concat_all_examples']
+    encoding_schemes = ['concat_all_examples', 'concat_each_example', 'sentence_level_segmentation']
     # ['-relu|mean', '-relu+|mean', 'relu+|mean', 'relu|mean', 'relu+|last', 'relu|last', '-relu+|last', 'relu+|last']
     # ['w1mean', 'relu|w1mean', '-relu|w1mean']  # ['-relu+|mean', '-relu+|last', '-relu|last']
     word_agg_schemes = ['w1mean']
-    segment_agg_schemes = [None]
+    segment_agg_schemes = ['mean']
     example_agg_schemes = ['mean']
     norms = [None]
     sim_funcs = ['dot_product']
     # ['middle', None, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
-    encoding_layers = [None, 'middle']  # ,23, 'E', 0, 'middle']
+    encoding_layers = [None]  # ,23, 'E', 0, 'middle']
     output_enclayer_and_aggschemes: list[tuple] = [(None, None)]  # [('OE', 'mean')]
     if 0 in num_fewshots:
         ALLOWED_ZEROSHOT_ENCODING_SCHEMES = {'concat_all_examples',
@@ -47,10 +47,26 @@ def run(overwrite_results: bool, NUM_GPUS_PER_RUN: int, cluster: str):
     for num_fewshot, (task, model), submodel, encoding_scheme, word_agg_scheme, segment_agg_scheme, example_agg_scheme, norm, sim_func, encoding_layer, (out_encoding_layer, out_word_agg_scheme) in itertools.product(
             num_fewshots, task_models, pretrained, encoding_schemes, word_agg_schemes, segment_agg_schemes, example_agg_schemes, norms, sim_funcs, encoding_layers, output_enclayer_and_aggschemes):
 
+        # Remove unnecessary combinations to help reign in combinatorial explosion
         if num_fewshot == 0:
+            if task.startswith('hellaswag'):
+                if encoding_scheme in ['concat_each_example', 'segment_each_example']:
+                    encoding_scheme = 'concat_all_examples'
+                if encoding_scheme == 'concat_all_examples':
+                    segment_agg_scheme = None
+                    example_agg_scheme = None
             if ((encoding_scheme not in ALLOWED_ZEROSHOT_ENCODING_SCHEMES)
                     or (example_agg_scheme not in ALLOWED_ZEROSHOT_EXAMPLE_AGG_SCHEMES)):
                 continue
+        else:
+            if task.startswith('hellaswag'):
+                if encoding_scheme in ['segment_each_example']:
+                    encoding_scheme = 'concat_each_example'
+                if encoding_scheme == 'concat_all_examples':
+                    segment_agg_scheme = None
+                    example_agg_scheme = None
+                elif encoding_scheme == 'concat_each_example':
+                    segment_agg_scheme = None
 
         _args = [
             "--device", 0,
@@ -76,7 +92,7 @@ def run(overwrite_results: bool, NUM_GPUS_PER_RUN: int, cluster: str):
 
         results_path = results_fpath(*_args)
         if (results_path is not None) and (not overwrite_results) and os.path.exists(results_path):
-            print(f'Skipping config:\n{_args}')
+            logging.info(f'Skipping config:\n{_args}')
         else:
             # Call the ray remote function, passing it the number of gpus to use
             if submodel is None:
